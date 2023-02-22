bd2572e8f244c36f7f4d948f45bb3673d8040b3c
Yesi Dedehidayati <yesi.223040179@mail.unpas.ac.id>
2023-02-22 17:45:57 +0700

    first commit

---
+++ kuliah/pertemuan2/index.php
@@ -0,0 +1,20 @@
+<?php
+$nama = 'Yesi';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>pendahuluan php</title>
+</head>
+<body>
+    
+<h1>
+    <?php echo "Hello $nama!"; ?>
+</h1>
+<p><?php echo "Sekarang hari jum'at"; ?></p>
+<p><?php echo 10 + '10' ?></p>
+</body>
+</html>
